0ab312bb51c14db32f4c7ac86da51bbadf5f937c
Tohir Ravshanov <ravshanovtohir11@gmail.com>
2022-05-13 04:29:24 +0500

    start branches

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,6 +3,7 @@ import { makeExecutableSchema } from '@graphql-tools/schema'
 import UserModule from './user/index.js'
 import StaffModule from './staff/index.js'
 import TypesModule from './types/index.js'
+// import BrancheModule from 
 
 export default makeExecutableSchema({
     typeDefs: [
